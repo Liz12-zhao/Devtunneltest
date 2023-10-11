910627d6ea5bc1075ba75090b8830846eeef98e3
ZYL <v-yanlzh@microsoft.com>
2023-10-11 02:15:48 -0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World2!");
+        Console.WriteLine("Hello, World3!");
+        Console.WriteLine("509");
+        
+    }
+}
